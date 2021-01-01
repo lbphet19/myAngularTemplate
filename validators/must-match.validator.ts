5b9264fcca718101485f0b0cddff5c9dd61e8dbc
lbphet19 <75687993+lbphet19@users.noreply.github.com>
2021-01-01 23:05:22 +0700

    Add files via upload

---
+++ validators/must-match.validator.ts
@@ -0,0 +1,18 @@
+import { FormGroup } from '@angular/forms';
+
+// custom validator to check that two fields match
+export function MustMatch(controlName: string, matchingControlName: string) {
+   return(formGroup:FormGroup) =>{
+     const control = formGroup.controls[controlName];
+     const matchingControl = formGroup.controls[matchingControlName];
+     // return if another validator has already found an error on the matchingControl
+     if(matchingControl.errors && !matchingControl.errors.MustMatch)
+      return;
+      // set error on matchingControl if validation fails
+      if(control.value !== matchingControl.value){
+        matchingControl.setErrors({MustMatch:true});
+      }
+      else matchingControl.setErrors(null);
+
+   }
+}
